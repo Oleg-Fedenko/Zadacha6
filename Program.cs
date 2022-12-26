684acbb5f0ee70c1140d64da564b43bcfa6396f5
Феденко Олег Алексеевич <o.fedenko@2gis.ru>
2022-12-26 08:54:08 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
             Console.WriteLine("Введите число:");
-            number = int.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
 
             if (number % 2 == 1)
             {
@@ -9,5 +9,5 @@
             }
             else
             {
-                Console.WriteLine("Число " + nummber + "является: чётным");
+                Console.WriteLine("Число " + number + "является: чётным");
             }
